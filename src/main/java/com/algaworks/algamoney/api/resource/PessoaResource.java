9ec78774c86bc89fae312ef04717f55c4a45cb5f
José Maria de Oliveira <jose.oliveira@synchro.com.br>
2018-06-21 16:56:17 -0300

    Remover uma pessoa

--- src/main/java/com/algaworks/algamoney/api/resource/PessoaResource.java
+++ src/main/java/com/algaworks/algamoney/api/resource/PessoaResource.java
@@ -42,4 +42,12 @@ public class PessoaResource {
 
 		return optional.isPresent() ? ResponseEntity.ok(optional.get()) : ResponseEntity.notFound().build();
 	}
+
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long codigo) {
+		pessoaRepository.deleteById(codigo);
+	}
+
+
 }
